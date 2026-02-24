58e08038920b8ea9d92f8e93dee0f897fb5f9e0b
kuchikamikaze <140504855+kuchikamikaze@users.noreply.github.com>
2026-02-24 18:21:55 +0800

    Add files via upload

---
+++ Pertemuan Pertama/luaspersegipanjang.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+float p,l,luas;
+cout << "Masukkan Panjang : " ;
+cin >> p;
+cout << "masukkan lebar : " ;
+cin >> l;
+luas = p*l;
+cout << "hasil nya = " << luas ;
+}
---
+++ Pertemuan Pertama/pembayaran.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  bool cek = true;
+  char jk;
+  int angka = 10;
+  float tinggi = 180.5;
+  double harga = 200000.00;
+  string  nama = "Naufal";
+
+  jk = 'L';
+  cout << "Selamat siang" << endl;
+  cout << "di ti umy\n";
+  cout << "yogyakarta\n";
+  
+  nama =  "Naufal";
+  cout << "nama saya : "  << nama;
+}
